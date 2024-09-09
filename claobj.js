fbe52196658d731f63f9449e2b6b301ecb5f1682
nitishsingamsetti <nsingamsetti11@gmail.com>
2024-09-09 18:19:03 +0530

    'inheritence'

--- claobj.js
+++ claobj.js
@@ -30,3 +30,71 @@ class realMadrid {
 let vini = new realMadrid(2,'brazil');
 let bellingoal = new realMadrid(1,'england');
 console.log(vini.trophies)
+
+//inheritence
+//properties and methods from parent carried to child class
+
+class Parent {
+    hello(){
+        console.log('hello')
+    }
+};
+
+class Child extends Parent{
+
+}
+
+let obj= new Child()
+console.log(obj.hello())
+
+// class Person {
+//     eat(){
+//         console.log('eat')
+//     }
+//     sleep(){
+//         console.log('sleep')
+//     }
+//     work(){
+//         console.log('do nothing')
+//     }
+// };
+
+// class Engineer extends Person{
+//     work(){
+//         console.log('build projects')
+//     }
+// };
+
+// let ramos = new Engineer();
+// console.log(ramos.eat())
+// console.log(ramos.sleep())
+// console.log(ramos.work())//child class functions get invoked not parent funstion
+// method overriding the work function
+
+//super();
+
+class Person {
+    constructor(){
+        console.log('enter parent constructor')
+        this.species='homo sapiens'
+    }
+    eat(){
+        console.log('eat')
+    }
+   
+};
+
+class Engineer extends Person{
+    constructor(branch){
+        console.log('enter child constructor');
+        super();//to invoke parent constructor
+        this.branch=branch;
+        console.log('exit child constuctor')
+    }
+    work(){
+        console.log('build projects')
+    }
+};
+
+let objnew = new Engineer()
+
